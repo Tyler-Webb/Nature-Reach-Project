0188218839fee1d0e9acc4ffccb3529eac5bd491
Tyler Webb <tgwebb@gus.pittstate.edu>
2020-10-17 22:37:14 -0500

    more fixes

--- controllers/feedings controller.js
+++ controllers/feedings controller.js
@@ -2,3 +2,7 @@
 exports.get_feedings = function(req, res) {
     res.render('feedings', {title: 'Feedings'});
 }
+
+exports.get_feedings_edit = function(req, res) {
+    res.render('feedingsedit', {title: 'Feedings Edit'});
+}
--- routes/feedings.js
+++ routes/feedings.js
@@ -3,6 +3,6 @@ var router = express.Router();
 let feedingsController = require('../controllers/feedings controller');
 /* GET home page. */
 router.get('/', feedingsController.get_feedings);
-
+router.get('/edit', feedingsController.get_feedings_edit);
 
 module.exports = router;
