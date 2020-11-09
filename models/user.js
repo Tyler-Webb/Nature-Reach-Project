44ad8a53736725b280ec5f761f929f25a6ab199d
Tyler Webb <tgwebb@gus.pittstate.edu>
2020-11-08 19:44:15 -0600

    added read and create functionality for users

--- models/user.js
+++ models/user.js
@@ -1,16 +1,5 @@
 const mongoose = require('mongoose');
 const bcrypt   = require('bcrypt-nodejs');
-
-//const { Schema } = mongoose;
-
-//const userSchema = new Schema({
-  //  firstName: { type: String, required: true},
- //   lastName: { type: String, required: true},
- //   email: { type: Boolean, required: true},
-//    roll: { type: String, required: true}
-//});
-
-// Create Schema
 const userSchema = new mongoose.Schema(
     {
         firstName: {
