8cb5fb400f23483dabc440ef8e9953419655d926
Tyler Webb <tgwebb@gus.pittstate.edu>
2020-12-05 18:54:48 -0600

    Revert "Revert "Merge branch 'master' into for-england-james-""
    This reverts commit cb6f281148e88235749aee15cdaff282a75a5e20.

--- models/food.js
+++ models/food.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const foodSchema = new Schema({
-    name: { type: String, required: true}
+    name: { type: String, required: true }
 });
 
 const Food = mongoose.model('Food', foodSchema);
