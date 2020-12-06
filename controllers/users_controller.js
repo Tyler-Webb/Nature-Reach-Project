3840f017226054a18c5fa422ae9281ab228ab440
WadeHand <whand@gus.pittstate.edu>
2020-12-05 18:14:24 -0600

    Fixed authentication to work
    I guess it didn't work the other way after all.

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,37 +1,49 @@
 const User = require('../models/user');
 
-exports.get_users = function(req, res) {
-    if (locals.user.role === "Admin") { 
-    User.find({}, function (err, users) {
-        if (err) {
-            console.error(err);
-        } else {
-            res.render('settings/users/users', { data: users });
-        }
-    })
+exports.get_users = function (req, res) {
+    let currentUser = res.locals.user;
+    if (currentUser.role === "Admin") {
+        User.find({}, function (err, users) {
+            if (err) {
+                console.error(err);
+            } else {
+                res.render('settings/users/users', { data: users });
+            }
+        })
+    } else {
+        res.render('error');
+        console.log('You do not have permission to this page.')
     }
 }
 
-exports.get_create_user = function(req,res) {
-    if (locals.user.role === "Admin") { 
-    res.render('settings/users/userscreate');
+exports.get_create_user = function (req, res) {
+    let currentUser = res.locals.user;
+    if (currentUser.role === "Admin") {
+        res.render('settings/users/userscreate');
+    } else {
+        res.render('error');
+        console.log('You do not have permission to this page.')
     }
 }
 
-exports.get_update_user = function(req,res) {
-    if (locals.user.role === "Admin") { 
-    User.findOne({ _id: req.query._id }, function (err, user) {
+exports.get_update_user = function (req, res) {
+    let currentUser = res.locals.user;
+    if (currentUser.role === "Admin") {
+        User.findOne({ _id: req.query._id }, function (err, user) {
 
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('settings/users/usersupdate', { data: user, title: 'Update User' });
-        }
-    });
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('settings/users/usersupdate', { data: user, title: 'Update User' });
+            }
+        });
+    } else {
+        res.render('error');
+        console.log('You do not have permission to this page.')
     }
 }
 
-exports.post_update_user = function(req,res) {
+exports.post_update_user = function (req, res) {
     const user = new User();
     const updateData = {
         email: req.body.email,
@@ -39,7 +51,7 @@ exports.post_update_user = function(req,res) {
         lastName: req.body.lastName,
         role: req.body.role
     };
-    
+
     if (req.body.password) {
         updateData.password = user.generateHash(req.body.password);
         updateData.passwordIsExpired = true
@@ -56,8 +68,8 @@ exports.post_update_user = function(req,res) {
     });
 }
 
-exports.post_create_user = function(req, res) {
-    
+exports.post_create_user = function (req, res) {
+
     let user = new User();
 
     user.email = req.body.email;
@@ -67,7 +79,7 @@ exports.post_create_user = function(req, res) {
     user.role = req.body.role;
     user.passwordIsExpired = true;
 
-    user.save(function(err) {
+    user.save(function (err) {
         if (err) {
             console.log(err);
         } else {
@@ -95,7 +107,7 @@ exports.export_users = async function (req, res) {
     return res.send(csv);
 }
 
-exports.delete_user = function(req,res) {
+exports.delete_user = function (req, res) {
     User.findOneAndDelete({ _id: req.query._id }, function (err) {
         if (err) {
             console.log(err);
